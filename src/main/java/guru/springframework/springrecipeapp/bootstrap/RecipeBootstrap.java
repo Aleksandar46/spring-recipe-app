de1becf43a046841c673b4781c68c8d62338c0c2
Aleksandar46 <aleksandarilievski62@gmail.com>
2020-07-11 02:10:28 +0200

    display remaining recipe properties

--- src/main/java/guru/springframework/springrecipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/springrecipeapp/bootstrap/RecipeBootstrap.java
@@ -137,7 +137,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
-
+        guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
         //add to return list
         recipes.add(guacRecipe);
 
@@ -196,6 +198,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         tacosRecipe.getCategories().add(americanCategory);
         tacosRecipe.getCategories().add(mexicanCategory);
+        tacosRecipe.setUrl("http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacosRecipe.setServings(4);
+        tacosRecipe.setSource("Simply Recipe");
 
         recipes.add(tacosRecipe);
         return recipes;
