e49b6a41d8abd5831e73bf122389708127b76d6f
Aleksandar46 <aleksandarilievski62@gmail.com>
2020-07-27 02:43:29 +0200

    view show ingredient

---
+++ src/main/java/guru/springframework/springrecipeapp/services/IngredientServiceImpl.java
@@ -0,0 +1,45 @@
+package guru.springframework.springrecipeapp.services;
+
+import guru.springframework.springrecipeapp.commands.IngredientCommand;
+import guru.springframework.springrecipeapp.converters.IngredientToIngredientCommand;
+import guru.springframework.springrecipeapp.domain.Recipe;
+import guru.springframework.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final RecipeRepository recipeRepository;
+
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if (!recipeOptional.isPresent()){
+            //todo impl error handling
+        }
+
+        Recipe recipe = recipeOptional.get();
+
+        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map( ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+
+        if(!ingredientCommandOptional.isPresent()){
+            //todo impl error handling
+        }
+
+        return ingredientCommandOptional.get();
+    }
+}
